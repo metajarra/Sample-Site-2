64aa5e221d1d2a5e9b911361bcd81301ae899395
John Sangster <john.sangster445@gmail.com>
2022-04-15 21:59:31 -0400

    test

--- app.py
+++ app.py
@@ -32,16 +32,16 @@ def geo():
 
 @app.route("/makepost")
 def makepost():
-    #filename = "characters_pickled"
-    #countfile = "character_count.txt"
+    filename = "characters_pickled"
+    countfile = "character_count.txt"
 
-    #infile = open(filename, "rb")
-    #oldCharacters = pickle.load(infile)
-    #infile.close
+    infile = open(filename, "rb")
+    oldCharacters = pickle.load(infile)
+    infile.close
 
-    #cfile = open(countfile, "r")
-    #currentcount = int(cfile.read())
-    #cfile.close()
+    cfile = open(countfile, "r")
+    currentcount = int(cfile.read())
+    cfile.close()
 
     return render_template("makepost.html")
 
