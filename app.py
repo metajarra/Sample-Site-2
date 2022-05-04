26728dadbb2b0b2780a305a87ad7ecd8f2b2fd4a
John Sangster <john.sangster445@gmail.com>
2022-05-03 23:31:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def writeToCharacters():
         tempchars = pickle.load(tempfile)
         tempfile.close()
 
-        rels.append(rel1 + ": " + rel2 + ": ")
+        rels.append(rel1 + ": " + rel2 + ": " + tempchars)
         relsID.append(rel2)
 
     # Write the new stuff to the appropriate file(s)
